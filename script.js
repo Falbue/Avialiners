5a4db68790753624f546159654dc51371fdd47a7
Falbue <cyansair05@gmail.com>
2023-04-10 01:41:27 +0300

    modify

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Кнопки добавления билетов
 let ticketCount = 0;
   
   function addTicket() {
@@ -13,3 +14,56 @@ let ticketCount = 0;
       document.getElementById("ticketCount").value = ticketCount;
     }
   }
+
+
+
+let passengers = 1; // количество пассажиров по умолчанию
+let maxPassengers = 5; // максимальное число пассажиров
+
+function addPassenger() {
+  if (passengers < maxPassengers) {
+    passengers++;
+    addPassengerFields();
+  } else {
+    alert("Мест для бронирования больше нет");
+  }
+}
+
+function removePassanger() {
+  if (passengers > 1) {
+    passengers--;
+    removePassengerFields();
+  } else {
+    alert("Удаление пассажиров из списка запрещено");
+  }
+}
+
+function addPassengerFields() {
+  // добавление полей ввода для нового пассажира
+}
+
+function removePassengerFields() {
+  // удаление полей ввода последнего пассажира
+}
+
+// привязка функций к кнопкам
+document.querySelector(".passager_num_buttons").addEventListener("click", function(event) {
+  if (event.target.tagName === "BUTTON") {
+    let btnValue = parseInt(event.target.textContent);
+    while (passengers !== btnValue) {
+      if (passengers > btnValue) {
+        removePassenger();
+      } else {
+        addPassenger();
+      }
+    }
+  }
+});
+
+document.querySelector(".frame_booking_buttons").addEventListener("click", function(event) {
+  if (event.target.textContent === "+") {
+    addPassenger();
+  } else if (event.target.textContent === "-") {
+    removePassenger();
+  }
+});
